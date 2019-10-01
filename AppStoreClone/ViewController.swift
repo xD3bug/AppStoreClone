5567ddf3c7e52435c42a5c89f36374a7d996e7b4
xD3bug <aslan.kunashev@gmail.com>
2019-10-02 00:45:45 +0300

    Initial Commit

---
+++ AppStoreClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by Аслан on 02/10/2019.
+//  Copyright © 2019 Doka.fun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
