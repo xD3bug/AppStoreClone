e8b0cc9940595e950279a765f9dc0c8a01a0a0fc
xD3bug <aslan.kunashev@gmail.com>
2019-10-02 02:16:30 +0300

    Commit before refactoring

--- AppStoreClone/BaseTabBarController.swift
+++ AppStoreClone/BaseTabBarController.swift
@@ -12,20 +12,29 @@ class BaseTabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("TabBar loaded")
         
         let redViewController = UIViewController()
-        redViewController.view.backgroundColor = .red
+        redViewController.view.backgroundColor = .white
+        redViewController.navigationItem.title = "Apps"
        // redViewController.tabBarItem.title = "Red"
         
         let redNavController = UINavigationController(rootViewController: redViewController)
         redNavController.tabBarItem.title = "Red Nav"
+        redNavController.tabBarItem.image = UIImage(named: "apps")
+        redNavController.navigationBar.prefersLargeTitles = true
         
         let blueViewController = UIViewController()
-        blueViewController.view.backgroundColor = .blue
-       // blueViewController.tabBarItem.title = "Blue"
+        blueViewController.view.backgroundColor = .white
+        blueViewController.navigationItem.title = "Search"
+        blueViewController.tabBarItem.title = "Blue"
+        blueViewController.tabBarItem.image = UIImage(named: "search")
         
         let blueNavController = UINavigationController(rootViewController: blueViewController)
         blueNavController.tabBarItem.title = "Blue Nav"
+        blueNavController.navigationBar.prefersLargeTitles = true
+        
+       // tabBar.barTintColor = .gray
         
         viewControllers = [
             redNavController,
