2935a16b709491bd9989175a591408f5c89bda97
xD3bug <aslan.kunashev@gmail.com>
2019-10-02 18:49:05 +0300

    Code refactoring

---
+++ AppStoreClone/Controller/BaseTabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  BaseTabBarController.swift
+//  AppStoreClone
+//
+//  Created by Аслан on 02/10/2019.
+//  Copyright © 2019 Doka.fun. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("TabBar loaded")
+        
+       // tabBar.barTintColor = .gray
+        
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+            createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+            createNavController(viewController: UIViewController(), title: "Search", imageName: "search")
+        ]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        
+        viewController.view.backgroundColor = .white
+        viewController.navigationItem.title = title
+        
+        return navController
+    }
+}
