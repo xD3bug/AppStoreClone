800d9d2b6907062264951ad5f9afb0e5c3540032
xD3bug <aslan.kunashev@gmail.com>
2019-10-13 22:52:08 +0300

    Nested collection view controller implemented

---
+++ AppStoreClone/Controller/New Group/AppsHorizontalController.swift
@@ -0,0 +1,52 @@
+//
+//  AppsHorizontalController.swift
+//  AppStoreClone
+//
+//  Created by Аслан on 13.10.2019.
+//  Copyright © 2019 Doka.fun. All rights reserved.
+//
+
+import UIKit
+
+class AppsHorizontalController: BaseCollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "id"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .blue
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    let topBottomPadding: CGFloat = 12
+    let lineSpacing: CGFloat = 10
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let height = (view.frame.height - topBottomPadding * 2 - lineSpacing * 2) / 3
+        return .init(width: view.frame.width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return lineSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: topBottomPadding, left: 16, bottom: topBottomPadding, right: 16)
+    }
+}
---
+++ AppStoreClone/Views/Apps/AppsGroupCell.swift
@@ -0,0 +1,42 @@
+//
+//  AppsGroupCell.swift
+//  AppStoreClone
+//
+//  Created by Аслан on 11.10.2019.
+//  Copyright © 2019 Doka.fun. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    convenience init(text: String, font: UIFont) {
+        self.init(frame: .zero)
+        self.text = text
+        self.font = font
+    }
+}
+
+class AppsGroupCell: UICollectionViewCell {
+    
+    let titleLabel = UILabel(text: "App Section", font: .boldSystemFont(ofSize: 30))
+    
+    let horizontalController = AppsHorizontalController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .lightGray
+        
+        addSubview(titleLabel)
+        titleLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor)
+        
+        addSubview(horizontalController.view)
+        
+        horizontalController.view.anchor(top: titleLabel.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
