1e932ee698fce1bf95816043729d57094501abde
xD3bug <aslan.kunashev@gmail.com>
2020-04-02 13:39:10 +0300

    removed old code from service layer

--- AppStoreClone/Service/Service.swift
+++ AppStoreClone/Service/Service.swift
@@ -61,47 +61,11 @@ class Service {
         fetchGenericJSONData(urlString: urlString, completion: completion)
     }
     
-//        guard let url = URL(string: urlString) else { return }
-//        URLSession.shared.dataTask(with: url) { (data, resp, err) in
-//            if let err = err {
-//                completion(nil, err)
-//                return
-//            }
-//
-//            do {
-//                let appGroup = try JSONDecoder().decode(AppGroup.self, from: data!)
-//               // appGroup.feed.results.forEach({ print($0.name) })
-//                completion(appGroup, nil)
-//            } catch {
-//                completion(nil, error)
-//            }
-//
-//        }.resume()
-//    }
-    
     func fetchSocialApps(completion: @escaping ([SocialApp]?, Error?) -> Void) {
         let urlString = "https://api.letsbuildthatapp.com/appstore/social"
         
         fetchGenericJSONData(urlString: urlString, completion: completion)
     }
-        
-//        guard let url = URL(string: urlString) else { return }
-//
-//        URLSession.shared.dataTask(with: url) { (data, resp, err) in
-//            if let err = err {
-//                completion(nil, err)
-//                return
-//            }
-//
-//            do {
-//                let objects = try JSONDecoder().decode([SocialApp].self, from: data!)
-//                completion(objects, nil)
-//            } catch {
-//                completion(nil, error)
-//            }
-//
-//        }.resume()
-//    }
     
     func fetchGenericJSONData<T: Decodable>(urlString: String, completion: @escaping (T?, Error?) -> ()) {
         
